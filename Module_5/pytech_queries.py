9e4ce2a7cc718c96529acd857161cf47d25f61ca
LongHat <95514425+LongHat@users.noreply.github.com>
2022-01-16 13:26:41 -0600

    Add files via upload

---
+++ Module_5/pytech_queries.py
@@ -0,0 +1,21 @@
+MongoDB: find()
+docs = db.collection_name.find({})
+
+for doc in docs:
+    print(doc)
+
+students.find({})
+
+MongoDB: find_one()
+doc = db.collection_name.find_one({"student_id": "1007"})
+
+MongoDB: find_one()
+doc = db.collection_name.find_one({"student_id": "1008"})
+
+MongoDB: find_one()
+doc = db.collection_name.find_one({"student_id": "1009"})
+
+print(doc["student_id"])
+
+print("\n -- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY -- ")
+print()
